0d343c744d0ed1ce9e98c2c937deaed5159dccbb
Alberto Ceballos <alcebal2002@yahoo.es>
2023-04-23 11:11:57 +0200

    Set global variables

--- src/main/java/com/example/CSVToJsonConverter.java
+++ src/main/java/com/example/CSVToJsonConverter.java
@@ -16,10 +16,14 @@ import org.slf4j.LoggerFactory;
 public class CSVToJsonConverter {
 
     static final Logger logger = LoggerFactory.getLogger(CSVToJsonConverter.class);
+    static String dataFileName = "csv_sample.csv";
+    static String formatFileName = "csv_format.json";
+
+    static String separator = "\\|";
+    static int rowTypePosition;
 
     public static void main(String[] args) {
-        String dataFileName = "csv_sample.csv";
-        String formatFileName = "csv_format.json";
+
         JSONObject outputJson = new JSONObject();
 
         ArrayList<String> contentArray = new ArrayList<String>();
@@ -42,6 +46,8 @@ public class CSVToJsonConverter {
 
         formatJson = new JSONObject(formatStringBuffer.toString());
         logger.info("formatJson: {}", formatJson);
+
+        rowTypePosition = formatJson.getJSONObject("data").getInt("rowTypePosition");
 
         // Read the csv file and count the number of lines of the file
         int numberOfLines = 0;
@@ -81,29 +87,32 @@ public class CSVToJsonConverter {
 
         logger.info(outputJson.toString(2));
         logger.info("data[0][first_field]: {}",
-                outputJson.getJSONArray("data").getJSONObject(0).get("type0-5").toString());
+                outputJson.getJSONArray("data").getJSONObject(0).get("5").toString());
     }
 
     private static JSONObject getJsonObjectFromLine(String line, JSONObject formatJson, String block) {
         JSONObject jsonObject = new JSONObject();
-        String[] dataValues = line.split("\\|");
+        String[] dataValues = line.split(separator);
         for (int i = 0; i < dataValues.length; i++) {
-            if ("header".equals(block)) {
+            if ("data".equals(block)) {
+                // data section contains rows of different types (with different lengths)
+                // the formatJson file can replace data row keys by specific names if they are
+                // defined in that file
+                jsonObject.put(
+                        (formatJson.getJSONObject(block)
+                                .has(dataValues[rowTypePosition])
+                                && formatJson.getJSONObject(block).getJSONObject(dataValues[rowTypePosition])
+                                        .has("" + i))
+                                                ? formatJson.getJSONObject(block)
+                                                        .getJSONObject(dataValues[rowTypePosition])
+                                                        .getString("" + i)
+                                                : ("" + i),
+                        dataValues[i]);
+            } else {
                 jsonObject.put(
                         (formatJson.getJSONObject(block).has("" + i))
                                 ? formatJson.getJSONObject(block).getString("" + i)
                                 : ("" + i),
-                        dataValues[i]);
-            } else if ("data".equals(block)) {
-                jsonObject.put(
-                        (formatJson.getJSONObject(block).getJSONObject(dataValues[0]).has("" + i))
-                                ? formatJson.getJSONObject(block).getJSONObject(dataValues[0]).getString("" + i)
-                                : ("" + i),
-                        dataValues[i]);
-            } else if ("trailer".equals(block)) {
-                jsonObject.put((formatJson.getJSONObject(block).has("" + i))
-                        ? formatJson.getJSONObject(block).getString("" + i)
-                        : ("" + i),
                         dataValues[i]);
             }
         }
